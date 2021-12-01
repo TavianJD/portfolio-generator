1ef3054bc6eb13a63ea011eca81922c85ebba190
hatetavian <tavianjdawson123@gmail.com>
2021-11-30 20:06:39 -0600

    getting data on page

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const inquirer = require('inquirer');
 const Choices = require('inquirer/lib/objects/choices');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-// // we interpolated the text using variables passed into the function that created the output.
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
-// fs.writeFile('index.html', pageHTML, err => {
-//    if (err) throw new  (err);
-
-//    console.log('Portfolio complete! Check out index.html to see the output!')
-// });
 
 const promptUser = () => {
 
@@ -147,5 +139,13 @@ const promptProject = portfolioData => {
 promptUser()
    .then(promptProject)
    .then(portfolioData => {
-      console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+      // we interpolated the text using variables passed into the function that created the output.
+
+
+       fs.writeFile('index.html', pageHTML, err => {
+          if (err) throw new  (err);
+
+         console.log('Portfolio complete! Check out index.html to see the output!')
+      });
    });
